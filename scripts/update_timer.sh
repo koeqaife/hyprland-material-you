55458d7c1a48fe331b8a0fe8e027c5be4fdc5a38
Danik <retikadni@gmail.com>
2024-06-21 03:36:25 +0200

    1.3.3

--- scripts/update_timer.sh
+++ scripts/update_timer.sh
@@ -1,9 +1,15 @@
 NOTIFICATION_SENT_FILE="/tmp/git_update_notification_sent"
 
-send_notification() {
+dotfiles_update() {
     notify-send -u critical --icon "software-update-available-symbolic"\
-    "Update available"\
+    "Dotfiles update available"\
     "If you haven't changed the dotfiles folder, you can write \n\$ ~/dotfiles/update.sh"
+}
+
+files_changed() {
+    notify-send -t 15000 -u critical --icon "software-update-urgent-symbolic"\
+    "Warning"\
+    "Hey you, yes you! If you see this notification, it means that some files in ~/dotfiles have been changed and you will no longer receive updates; if this is a bug, please report it to the developer."
 }
 
 check_and_notify_update() {
@@ -13,9 +19,12 @@ check_and_notify_update() {
 
     if [ $status -eq 2 ]; then
         if ! [ -f "$NOTIFICATION_SENT_FILE" ]; then
-            send_notification
+            dotfiles_update
             touch "$NOTIFICATION_SENT_FILE"
         fi
+    elif [ $status -eq 1 ]; then
+        files_changed
+        exit 1
     fi
 }
 
