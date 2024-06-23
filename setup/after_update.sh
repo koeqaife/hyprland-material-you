55098758eeb714e0eb4f5287ed0916d56182e3e9
Danik <retikadni@gmail.com>
2024-06-23 14:16:24 +0200

    GTK window buttons

--- setup/after_update.sh
+++ setup/after_update.sh
@@ -2,5 +2,8 @@
 
 "$HOME"/dotfiles/install.sh packages
 
+echo ":: Remove window close and minimize buttons in GTK"
+gsettings set org.gnome.desktop.wm.preferences button-layout ':'
+
 echo "Nothing to do"
 exit 0
