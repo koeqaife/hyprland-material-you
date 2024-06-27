ebd1ae767f29e0dbb0fa44617a619671f144351e
Danik <retikadni@gmail.com>
2024-06-28 01:29:59 +0200

    Theme and wallpaper settings

--- ags/apps/settings/main.ts
+++ ags/apps/settings/main.ts
@@ -1,6 +1,8 @@
 import { RegularWindow } from "apps/window";
 import { Network } from "./network";
 import { Bluetooth } from "./bluetooth";
+import { Theme } from "./theme";
+import { Wallpapers } from "./wallpapers";
 import { Widget as WidgetType } from "types/widgets/widget";
 const hyprland = await Service.import("hyprland")
 import Gtk from "gi://Gtk?version=3.0"
@@ -38,7 +40,9 @@ function Settings(cur_tab: string) {
         transition: "crossfade",
         children: {
             "network": Page(Network(), "Network"),
-            "bluetooth": Page(Bluetooth(), "Bluetooth")
+            "bluetooth": Page(Bluetooth(), "Bluetooth"),
+            "theme": Page(Theme(), "Theme"),
+            "wallpaper": Page(Wallpapers(), "Wallpapers")
         }
     })
     const Row = (name: string, label: string, icon: string = "image-missing") => Widget.Button({
@@ -59,7 +63,10 @@ function Settings(cur_tab: string) {
         children: [
             Row("network", "Network", "network-wireless-signal-excellent-symbolic"),
             Row("bluetooth", "Bluetooth", "bluetooth-active-symbolic"),
-            Widget.Separator()
+            Widget.Separator(),
+            Row("theme", "Theme", "applications-graphics-symbolic"),
+            Row("wallpaper", "Wallpapers", "folder-pictures-symbolic"),
+            Widget.Separator(),
         ]
     })
     return Widget.Box({
