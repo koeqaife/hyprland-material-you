b8dbebe213c7dd7945754fb65a4f1fa2d9d6a8a6
Danik <retikadni@gmail.com>
2024-06-28 01:25:29 +0200

    Current wallpaper variable

--- ags/variables.ts
+++ ags/variables.ts
@@ -2,6 +2,7 @@ const GLib = imports.gi.GLib;
 
 export const theme = Variable("dark");
 export const main_color = Variable("#000000");
+export const current_wallpaper = Variable(`${GLib.get_home_dir()}/dotfiles/wallpapers/default.png`);
 
 export const idle_inhibitor = Variable(false);
 export const night_light = Variable(false);
@@ -22,5 +23,20 @@ Utils.monitorFile(
         Utils.readFileAsync(color_scheme_file)
             .then(out => { theme.setValue(out); })
             .catch(err => { });
+    }
+)
+const wallpaper_cache_file = `${GLib.get_home_dir()}/.cache/current_wallpaper`
+Utils.readFileAsync(wallpaper_cache_file)
+    .then(out => { current_wallpaper.setValue(out.trim()); })
+    .catch(err => {
+        Utils.writeFileSync(current_wallpaper.value, wallpaper_cache_file)
+    });
+
+Utils.monitorFile(
+    wallpaper_cache_file,
+    () => {
+        Utils.readFileAsync(wallpaper_cache_file)
+            .then(out => { current_wallpaper.setValue(out); })
+            .catch(print);
     }
 )
