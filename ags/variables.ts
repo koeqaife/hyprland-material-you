5be3b403b38dd8500e3d4370384ac18a8b6867ca
Danik <retikadni@gmail.com>
2024-06-28 22:10:05 +0200

    New variables

--- ags/variables.ts
+++ ags/variables.ts
@@ -9,28 +9,77 @@ export const night_light = Variable(false);
 export const bluetooth_enabled = Variable('off', {
     poll: [1000, `${App.configDir}/scripts/bluetooth.sh --get`]
 })
+export const theme_settings = {
+    "color": Variable("none"),
+    "scheme": Variable("tonalSpot")
+}
 
+export const custom_color_file = `${GLib.get_home_dir()}/dotfiles/.settings/custom-color`
+export const generation_scheme_file = `${GLib.get_home_dir()}/dotfiles/.settings/generation-scheme`
+export const color_scheme_file = `${GLib.get_home_dir()}/dotfiles/.settings/color-scheme`
+export const wallpaper_cache_file = `${GLib.get_home_dir()}/.cache/current_wallpaper`
 
-const color_scheme_file = `${GLib.get_home_dir()}/dotfiles/.settings/color-scheme`
-Utils.readFileAsync(color_scheme_file)
-    .then(out => { theme.setValue(out); })
-    .catch(err => { });
+function readFiles() {
+    Utils.readFileAsync(custom_color_file)
+        .then(out => {
+            let _out = out;
+            if (_out.startsWith("-")) {
+                _out = _out.substring(1);
+            }
+            theme_settings.color.setValue(_out.trim());
+        })
+        .catch(print)
 
+    Utils.readFileAsync(generation_scheme_file)
+        .then(out => {
+            theme_settings.scheme.setValue(out);
+        })
+        .catch(print)
+
+    Utils.readFileAsync(color_scheme_file)
+        .then(out => { theme.setValue(out.trim()); })
+        .catch(err => { });
+
+    Utils.readFileAsync(wallpaper_cache_file)
+        .then(out => { current_wallpaper.setValue(out.trim()); })
+        .catch(err => {
+            Utils.writeFileSync(current_wallpaper.value, wallpaper_cache_file)
+        });
+}
+readFiles()
+
+Utils.monitorFile(
+    custom_color_file,
+    () => {
+        Utils.readFileAsync(custom_color_file)
+            .then(out => {
+                let _out = out;
+                if (_out.startsWith("-")) {
+                    _out = _out.substring(1);
+                }
+                theme_settings.color.setValue(_out.trim());
+            })
+            .catch(err => { });
+    }
+)
+
+Utils.monitorFile(
+    generation_scheme_file,
+    () => {
+        Utils.readFileAsync(generation_scheme_file)
+            .then(out => { theme_settings.scheme.setValue(out.trim()); })
+            .catch(err => { });
+    }
+)
 
 Utils.monitorFile(
     color_scheme_file,
     () => {
         Utils.readFileAsync(color_scheme_file)
-            .then(out => { theme.setValue(out); })
+            .then(out => { theme.setValue(out.trim()); })
             .catch(err => { });
     }
 )
-const wallpaper_cache_file = `${GLib.get_home_dir()}/.cache/current_wallpaper`
-Utils.readFileAsync(wallpaper_cache_file)
-    .then(out => { current_wallpaper.setValue(out.trim()); })
-    .catch(err => {
-        Utils.writeFileSync(current_wallpaper.value, wallpaper_cache_file)
-    });
 
 Utils.monitorFile(
     wallpaper_cache_file,
