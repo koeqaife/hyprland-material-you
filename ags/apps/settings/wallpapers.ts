1ea5cac41d591a9ae0cfea6bcb20ff5f6be9b477
Danik <retikadni@gmail.com>
2024-06-28 22:12:53 +0200

    Cache thumbnails on start

--- ags/apps/settings/wallpapers.ts
+++ ags/apps/settings/wallpapers.ts
@@ -106,9 +106,29 @@ const Wallpaper = (image: string, name: string) => Widget.Button({
     })
 })
 
-const WallpaperList = () => {
+function CacheThumbnails() {
     Utils.execAsync(`mkdir -p ${GLib.get_home_dir()}/.cache/thumbnails/wallpaper`)
-        .then()
+        .catch(print)
+    const link_directory = `${App.configDir}/wallpaper`;
+    const original_directory = `${GLib.get_home_dir()}/wallpaper`;
+    const extensions = [".jpg", ".jpeg", ".png"];
+    const fileList = listFilesByExtensions(link_directory, extensions);
+    fileList.forEach((value, index) => {
+        const path = `${original_directory}/${value}`
+        const cache_file = `${GLib.get_home_dir()}/.cache/thumbnails/wallpaper/${value}`;
+        Utils.idle(() => {
+            Utils.readFileAsync(cache_file)
+                .catch(() => {
+                    Utils.execAsync(`magick ${path} -resize x32 -gravity Center -extent 1:1 ${cache_file}`)
+                        .catch(print)
+                })
+        })
+    })
+}
+
+CacheThumbnails()
+
+const WallpaperList = () => {
     const link_directory = `${App.configDir}/wallpaper`;
     const original_directory = `${GLib.get_home_dir()}/wallpaper`;
     const extensions = [".jpg", ".jpeg", ".png"];
@@ -134,9 +154,7 @@ const WallpaperList = () => {
         ]
     })
 
-    Utils.interval(30000, () => {
-        box.attribute.update(box)
-    }, box)
+    box.attribute.update(box)
 
     return box
 }
