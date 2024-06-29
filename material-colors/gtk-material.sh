dfffc3d19d230f100fbf40431892b10839de2640
Danik <retikadni@gmail.com>
2024-06-29 16:41:08 +0200

    Change icon theme

--- material-colors/gtk-material.sh
+++ material-colors/gtk-material.sh
@@ -2,9 +2,10 @@
 
 if [ "$1" == "--light" ]; then
   gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3
+  gsettings set org.gnome.desktop.interface icon-theme "Tela-nord-light"
   gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
 else
   gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
   gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-
+  gsettings set org.gnome.desktop.interface icon-theme "Tela-nord-dark"
 fi
