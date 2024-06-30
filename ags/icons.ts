c1e0f330ae3e57ecac4265345312c8175f1d3b4d
Danik <retikadni@gmail.com>
2024-06-30 23:40:03 +0200

    Material Icons

---
+++ ags/icons.ts
@@ -0,0 +1,10 @@
+import { LabelProps } from "types/widgets/label"
+
+const _icons_folder = `${App.configDir}/assets/icons`
+
+export const MaterialIcon = (icon: string, size: string = "24px", props?: LabelProps) => Widget.Label({
+    label: icon,
+    class_name: "icon material_icon",
+    css: `font-size: ${size};`,
+    ...props
+})
