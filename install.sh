71fafc7c7023190126bbf98037bb01fd4e90f764
Danik <retikadni@gmail.com>
2024-07-01 18:48:22 +0200

    Hmm

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ yay -S --noconfirm --needed \
     libdrm mesa fwupd rofi-wayland bun-bin pipewire wireplumber udiskie \
     lm_sensors gnome-system-monitor playerctl ttf-meslo-nerd ttf-google-sans \
     ttf-font-awesome ttf-opensans ttf-roboto lshw ttf-material-symbols-variable-git \
-    fontconfig python-pywlroots
+    fontconfig
 
 sleep 2
 
