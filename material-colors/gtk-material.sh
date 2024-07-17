c10becda2fd9211adff36dd99f8746b6296ce59b
koeqaife <83244543+koeqaife@users.noreply.github.com>
2024-07-18 01:30:23 +0200

    use absolute path

--- material-colors/gtk-material.sh
+++ material-colors/gtk-material.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-ICON_THEME_DARK=$(sed -n "1p" .settings/icon-theme)
-ICON_THEME_LIGHT=$(sed -n "2p" .settings/icon-theme)
-
+ICON_THEME_DARK=$(sed -n "1p" "$HOME"/dotfiles/.settings/icon-theme)
+ICON_THEME_LIGHT=$(sed -n "2p" "$HOME"/dotfiles/.settings/icon-theme)
 
 if [ "$1" == "--light" ]; then
-    gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3
-    gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
-    gsettings set org.gnome.desktop.interface icon-theme "$ICON_THEME_LIGHT"
+  gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3
+  gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
+  gsettings set org.gnome.desktop.interface icon-theme "$ICON_THEME_LIGHT"
 else
-    gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
-    gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-    gsettings set org.gnome.desktop.interface icon-theme "$ICON_THEME_DARK"
+  gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
+  gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+  gsettings set org.gnome.desktop.interface icon-theme "$ICON_THEME_DARK"
 fi
