b75831f818ccdb5f3973b9c1e1ac88fadbd170a0
Danik <retikadni@gmail.com>
2024-07-29 20:50:31 +0200

    Convert settings script

--- setup/after_update.sh
+++ setup/after_update.sh
@@ -21,8 +21,8 @@ install_microtex() {
     makepkg -si
 }
 
-
 cp -f $HOME/dotfiles/setup/wl-gammarelay.service $HOME/.config/systemd/user/
+ask_continue "Import the old settings into the new settings?" && python "$HOME"/dotfiles/setup/import_settings.py
 ask_continue "Proceed with installing/updating packages?" && "$HOME"/dotfiles/install.sh packages
 ask_continue "Proceed with installing MicroTex?" && install_microtex
 
