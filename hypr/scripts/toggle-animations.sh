d6704a045c866b09add1fb0d965743e4be16a2f8
Danik <retikadni@gmail.com>
2024-08-03 16:52:34 +0200

    Disable animations in ags

--- hypr/scripts/toggle-animations.sh
+++ hypr/scripts/toggle-animations.sh
@@ -2,8 +2,10 @@
 cache_file="/tmp/toggle_animation"
 if [ -f $cache_file ]; then
     hyprctl keyword animations:enabled true
+    ags -r 'enableAnimations(true)'
     rm $cache_file
 else
     hyprctl keyword animations:enabled false
+    ags -r 'enableAnimations(false)'
     touch $cache_file
 fi
