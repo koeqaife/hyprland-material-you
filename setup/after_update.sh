68b867cea7bd02bb66cf098488fac8d4d0a9ec83
koeqaife <83244543+koeqaife@users.noreply.github.com>
2024-09-21 12:49:34 +0200

    Just some improvements

--- setup/after_update.sh
+++ setup/after_update.sh
@@ -3,13 +3,14 @@
 BACKUP_DIR="$HOME/dotfiles/.backup"
 
 FILES_TO_RESTORE=(
-    "./hypr/conf/custom/*"
-    "./.settings/*"
-    "./hypr/conf/apps.conf"
-    "./wallpapers/*"
-    "./hypr/hypridle.conf"
-    "./ags/README.md"
-    "./ags/tsconfig.json"
+    "$HOME/dotfiles/hypr/conf/custom/*"
+    "$HOME/dotfiles/.settings/*"
+    "$HOME/dotfiles/hypr/conf/apps.conf"
+    "$HOME/dotfiles/wallpapers/*"
+    "$HOME/dotfiles/hypr/hypridle.conf"
+    "$HOME/dotfiles/electron-flags.conf"
+    "$HOME/dotfiles/ags/README.md"
+    "$HOME/dotfiles/ags/tsconfig.json"
 )
 
 ask_continue() {
@@ -35,23 +36,34 @@ install_microtex() {
 
 restore_files() {
     for file_pattern in "${FILES_TO_RESTORE[@]}"; do
-        for file in $BACKUP_DIR/$file_pattern; do
+        for file in $BACKUP_DIR/$(basename "$file_pattern"); do
+            original_path="${file/$BACKUP_DIR\//}"
             if [ -e "$file" ]; then
-                original_path="${file/$BACKUP_DIR\//}"
                 cp "$file" "$original_path"
                 echo "Restored: $original_path"
-                git update-index --skip-worktree "$original_path"
             fi
         done
     done
 }
 
+skip_worktree() {
+    for file_pattern in "${FILES_TO_RESTORE[@]}"; do
+        git update-index --skip-worktree $file_pattern 2>/dev/null || echo "Unable to mark: $file_pattern"
+    done
+}
+
+copy_files() {
+    "$HOME/dotfiles/setup/copy.sh"
+}
+
 git ls-files -v | grep '^S' | awk '{print $2}' | xargs git update-index --no-skip-worktree
 restore_files
+skip_worktree
 
-cp -f $HOME/dotfiles/setup/wl-gammarelay.service $HOME/.config/systemd/user/
-ask_continue "Import the old settings into the new settings?" && python "$HOME"/dotfiles/setup/import_settings.py
-ask_continue "Proceed with installing/updating packages?" && "$HOME"/dotfiles/install.sh packages
+cp -f "$HOME/dotfiles/setup/wl-gammarelay.service" "$HOME/.config/systemd/user/"
+ask_continue "Import the old settings into the new settings?" && python "$HOME/dotfiles/setup/import_settings.py"
+ask_continue "Proceed with installing/updating packages?" && "$HOME/dotfiles/install.sh packages"
 ask_continue "Proceed with installing MicroTex?" && install_microtex
+ask_continue "Proceed to copy files?" && copy_files
 
 exit 0
