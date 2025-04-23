cb08f7a5f4a12b44c6b6f70f1968066c026badaf
enessmr <136172887+enessmr@users.noreply.github.com>
2025-04-23 16:37:12 +0300

    AMD CPU compatiblity

--- ags/scripts/system.sh
+++ ags/scripts/system.sh
@@ -15,7 +15,7 @@ get_swap_usage() {
 }
 
 get_cpu_temp() {
-    sensors | grep -i 'core ' | awk '{sum+=$3; count+=1} END {if (count > 0) print sum/count; else print "Undefined"}'
+    sensors | grep -i 'Tctl' | awk '{sum+=$2; count+=1} END {if (count > 0) print sum/count; else print "Undefined"}' || sensors | grep -i 'core ' | awk '{sum+=$3; count+=1} END {if (count > 0) print sum/count; else print "Undefined"}'
 }
 
 get_cpu_name() {
