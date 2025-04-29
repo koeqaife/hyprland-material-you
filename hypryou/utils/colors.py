56aff53d768f14738af1923f50bb665f27adf922
Danik <retikadni@gmail.com>
2025-04-29 19:23:28 +0200

    Add material colors generation

---
+++ hypryou/utils/colors.py
@@ -0,0 +1,443 @@
+import os
+import json
+from PIL import Image
+from materialyoucolor.quantize import QuantizeCelebi  # type: ignore
+from materialyoucolor.score.score import Score  # type: ignore
+from materialyoucolor.hct import Hct  # type: ignore
+from materialyoucolor.dynamiccolor.material_dynamic_colors import DynamicColor  # type: ignore # noqa
+from materialyoucolor.dynamiccolor.material_dynamic_colors import MaterialDynamicColors  # noqa
+from materialyoucolor.scheme.dynamic_scheme import DynamicScheme  # type: ignore # noqa
+from materialyoucolor.scheme.scheme_tonal_spot import SchemeTonalSpot  # type: ignore # noqa
+import hashlib
+import pickle
+import numpy as np
+import re
+import typing as t
+from config import APP_CACHE_PATH, CONFIG_DIR
+
+
+# I dropped support of color schemes
+# Because it's just easier when there's only 1 of them
+
+
+join = os.path.join
+
+TEMPLATES_DIR = join(CONFIG_DIR, "assets", "templates")
+CACHE_PATH = join(APP_CACHE_PATH, "colors")
+
+
+type IntFloat = int | float
+type RGB = tuple[IntFloat, IntFloat, IntFloat]
+type RGBA = tuple[IntFloat, IntFloat, IntFloat, IntFloat]
+
+
+def rgb_to_hex(rgb: RGB) -> str:
+    return '#{:02x}{:02x}{:02x}'.format(*rgb[:3])
+
+
+def rgba_to_rgb(rgba: RGBA) -> str:
+    return f'{rgba[0]}, {rgba[1]}, {rgba[2]}'
+
+
+def get_color(scheme: DynamicScheme, color: str) -> DynamicColor:
+    color = getattr(MaterialDynamicColors, color)
+    if isinstance(color, DynamicColor):
+        return color
+    else:
+        raise TypeError(f"Attribute '{color}' in scheme isn't a color")
+
+
+class Color():
+    def __init__(self) -> None:
+        pass
+
+    def __call__(self, name: str) -> str:
+        color_name: DynamicColor = getattr(MaterialDynamicColors, name)
+        if hasattr(color_name, "get_hct"):
+            return rgb_to_hex(color_name.get_hct(SchemeTonalSpot).to_rgba())
+        else:
+            return "#000000"
+
+
+class ColorsCache:
+    def __init__(
+        self,
+        colors: DynamicScheme | dict[str, str],
+        wallpaper: str | None,
+        original_color: int | None,
+        contrast_level: int,
+        is_dark: bool
+    ) -> None:
+        self.colors: dict[str, str] = {}
+        self.wallpaper = wallpaper
+        self.original_color = original_color
+        self.contrast_level = contrast_level
+        self.is_dark = is_dark
+        if isinstance(colors, DynamicScheme):
+            for color_name in vars(MaterialDynamicColors).keys():
+                color = get_color(colors, color_name)
+                self.colors[str(color)] = rgb_to_hex(
+                    color.get_hct(colors).to_rgba()
+                )
+        else:
+            self.colors = colors
+
+
+def colors_dict(cache: ColorsCache) -> dict[str, t.Any]:
+    dict = {
+        "wallpaper": cache.wallpaper,
+        "colors": cache.colors,
+        "original_color": cache.original_color,
+        "contrast_level": cache.contrast_level,
+        "is_dark": cache.is_dark
+    }
+    return dict
+
+
+def get_cache_object(object: dict[str, t.Any] | str) -> ColorsCache:
+    if isinstance(object, str):
+        object = dict(json.loads(object))
+
+    colors = object["colors"]
+    wallpaper = object["wallpaper"]
+    original_color = object["original_color"]
+    contrast_level = object.get("contrast_level", 0)
+    is_dark = object["is_dark"]
+
+    return ColorsCache(
+        colors,
+        wallpaper,
+        original_color,
+        contrast_level,
+        is_dark
+    )
+
+
+def get_file_list(folder_path: str) -> list[str]:
+    file_list: list[str] = []
+    for file in os.listdir(folder_path):
+        if os.path.isfile(join(folder_path, file)):
+            file_list.append(join(folder_path, file))
+    return file_list
+
+
+ready_templates = {
+    "colors.css": "@define-color {name} {hex};\n",
+    "colors.scss": "${name}: {hex};\n"
+}
+additional = {
+    "onBackground": "foreground"
+}
+
+
+def generate_color_map(scheme: DynamicScheme) -> dict[str, str]:
+    color_map: dict[str, str] = {}
+    for color in vars(MaterialDynamicColors).keys():
+        color_name = getattr(MaterialDynamicColors, color)
+        if hasattr(color_name, "get_hct"):
+            rgba = color_name.get_hct(scheme).to_rgba()
+            color_map[color.strip()] = rgb_to_hex(rgba)
+    return color_map
+
+
+class ColorFormatter:
+    def __init__(self, scheme: DynamicScheme) -> None:
+        self.color_map = generate_color_map(scheme)
+
+    def apply_transformations(
+        self,
+        value: str,
+        transformations: list[str]
+    ) -> str:
+        intermediate_transforms = [
+            t for t in transformations
+            if not (t.startswith("strip") or t == "rgb")
+        ]
+        final_transforms = [
+            t for t in transformations
+            if t.startswith("strip") or t == "rgb"
+        ]
+
+        for transform in intermediate_transforms:
+            if transform.startswith("lighten"):
+                matched = re.search(r'\d+', transform)
+                if not matched:
+                    continue
+                percent = int(matched.group())
+                value = self.adjust_brightness(value, percent)
+            elif transform.startswith("darken"):
+                matched = re.search(r'\d+', transform)
+                if not matched:
+                    continue
+                percent = int(matched.group())
+                value = self.adjust_brightness(value, -percent)
+
+        for transform in final_transforms:
+            if transform.startswith("strip"):
+                value = value.lstrip('#')
+            elif transform == "rgb":
+                value = self.hex_to_rgb(value)
+
+        return value
+
+    def adjust_brightness(
+        self,
+        hex_color: str,
+        factor: IntFloat
+    ) -> str:
+        def min_max(v: int) -> int:
+            return min(255, max(0, v))
+
+        hex_color = hex_color.lstrip('#')
+        r, g, b = (int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+
+        r = min_max(int(r * (1 + factor / 100)))
+        g = min_max(int(g * (1 + factor / 100)))
+        b = min_max(int(b * (1 + factor / 100)))
+
+        return f'#{r:02X}{g:02X}{b:02X}'.lower()
+
+    def hex_to_rgb(
+        self,
+        hex_color: str
+    ) -> str:
+        hex_color = hex_color.lstrip('#')
+        r, g, b = (int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+        return f'{r},{g},{b}'
+
+    def parse_transformations(
+        self,
+        transformations_str: str
+    ) -> list[str]:
+        transformations = re.findall(
+            r'(\w+)(?:\((\d*)\))?', transformations_str
+        )
+        result = []
+        for command, arg in transformations:
+            if command:
+                if arg:
+                    result.append(f"{command}({arg})")
+                else:
+                    result.append(command)
+        return result
+
+    def format(
+        self,
+        text: str
+    ) -> str:
+        pattern = r'(<(\w+)(?:\.(.+))?>)'
+        matches = re.findall(pattern, text)
+        result = []
+        last_end = 0
+
+        for match in matches:
+            full_match, key, transformations_str = match
+            if key in self.color_map:
+                result.append(text[last_end:text.find(full_match, last_end)])
+                value = self.color_map[key]
+                if transformations_str:
+                    transformations = self.parse_transformations(
+                        transformations_str
+                    )
+                    value = self.apply_transformations(value, transformations)
+                result.append(value)
+                last_end = text.find(full_match, last_end) + len(full_match)
+
+        result.append(text[last_end:])
+        str_result = ''.join(result)
+        pattern = r'<\\\\([^>]+)>'
+
+        str_result = re.sub(pattern, r'<\1>', str_result)
+
+        return str_result
+
+
+def generate_templates(
+    folder: str,
+    output_folder: str,
+    scheme: DynamicScheme,
+    is_dark: bool,
+    wallpaper: str | None = None
+) -> None:
+    color_scheme = "dark" if is_dark else "light"
+
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    file_list = get_file_list(folder)
+
+    for file_path in file_list:
+        with open(file_path) as f:
+            template = f.read()
+        formatter = ColorFormatter(scheme)
+        template = formatter.format(template)
+        template = (
+            template
+            .replace("<color-scheme>", color_scheme)
+            .replace("<output-folder>", output_folder)
+            .replace("<wallpaper>", wallpaper or "")
+        )
+        new_path = join(output_folder, os.path.basename(file_path))
+        with open(new_path, 'w') as f:
+            f.write(template)
+
+    for file in ready_templates:
+        _template = ""
+        for color in vars(MaterialDynamicColors).keys():
+            color_name = getattr(MaterialDynamicColors, color)
+            if hasattr(color_name, "get_hct"):
+                rgba = color_name.get_hct(scheme).to_rgba()
+                hex_color = rgb_to_hex(rgba)
+                rgb_color = rgba_to_rgb(rgba)
+                new_line = ready_templates[file].format(
+                    name=color,
+                    hex=hex_color,
+                    rgb=rgb_color
+                )
+                _template += new_line
+                if color in additional:
+                    new_line = ready_templates[file].format(
+                        name=additional[color],
+                        hex=hex_color,
+                        rgb=rgb_color
+                    )
+                    _template += new_line
+
+        new_path = join(output_folder, os.path.basename(file))
+        with open(new_path, 'w') as f:
+            f.write(_template)
+
+
+def process_image(
+    image_path: str,
+    quality: int = 2,
+    num_colors: int = 128
+) -> int:
+    def get_cache_path(image_path: str) -> str:
+        cache_path = join(CACHE_PATH, "cached_colors")
+        hash_object = hashlib.md5(image_path.encode())
+        cache_filename = hash_object.hexdigest() + '.pkl'
+        os.makedirs(cache_path, exist_ok=True)
+        return join(cache_path, cache_filename)
+
+    def load_from_cache(cache_path: str) -> t.Any:
+        if os.path.exists(cache_path):
+            with open(cache_path, 'rb') as f:
+                return pickle.load(f)
+        return None
+
+    def save_to_cache(cache_path: str, data: t.Any) -> None:
+        with open(cache_path, 'wb') as f:
+            pickle.dump(data, f)
+
+    cache_path = get_cache_path(image_path)
+
+    cached_result = load_from_cache(cache_path)
+    if cached_result is not None:
+        return int(cached_result)
+
+    image = Image.open(image_path).convert('RGB')
+
+    image_data = np.array(image)
+    pixel_array = image_data[::quality, ::quality].reshape(-1, 3)
+
+    result = QuantizeCelebi(pixel_array, num_colors)
+
+    color = int(Score.score(result)[0])
+
+    save_to_cache(cache_path, color)
+
+    return color
+
+
+@t.overload
+def generate_colors(
+    image_path: str,
+    use_color: t.Literal[None] = None,
+    is_dark: bool = True,
+    contrast_level: int = 0
+) -> None:
+    ...
+
+
+@t.overload
+def generate_colors(
+    image_path: t.Literal[None],
+    use_color: int,
+    is_dark: bool = True,
+    contrast_level: int = 0
+) -> None:
+    ...
+
+
+def generate_colors(
+    image_path: str | None = None,
+    use_color: int | None = None,
+    is_dark: bool = True,
+    contrast_level: int = 0
+) -> None:
+    if use_color is None and image_path is not None:
+        color = process_image(image_path, 4, 1024)
+    elif use_color is not None and image_path is None:
+        color = use_color
+    else:
+        raise TypeError("Either image_path or use_color should be not None.")
+
+    scheme = SchemeTonalSpot(
+        Hct.from_int(color),
+        is_dark,
+        contrast_level
+    )
+
+    with open(join(CACHE_PATH, "colors.json"), 'w') as f:
+        object = ColorsCache(
+            scheme, image_path, color, contrast_level, is_dark
+        )
+        json.dump(colors_dict(object), f, indent=2)
+
+    generate_templates(
+        TEMPLATES_DIR, CACHE_PATH, scheme, is_dark, image_path
+    )
+
+
+def generate_by_last_wallpaper() -> None:
+    try:
+        with open(join(CACHE_PATH, "colors.json")) as f:
+            content = get_cache_object(f.read())
+        assert content.wallpaper is not None
+        generate_colors(
+            content.wallpaper,
+            None,
+            content.is_dark,
+            contrast_level=content.contrast_level
+        )
+    except (FileNotFoundError, AssertionError):
+        generate_colors(
+            None,
+            0x0000FF,
+            True,
+            0
+        )
+
+
+def restore_palette() -> None:
+    try:
+        with open(join(CACHE_PATH, "colors.json")) as f:
+            content = get_cache_object(f.read())
+        assert content.original_color is not None
+        generate_colors(
+            content.wallpaper,
+            content.original_color,
+            content.is_dark,
+            content.contrast_level
+        )
+    except (FileNotFoundError, AssertionError):
+        generate_colors(
+            None,
+            0x0000FF,
+            True,
+            0
+        )
