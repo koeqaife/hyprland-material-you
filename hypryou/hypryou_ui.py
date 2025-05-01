40b92309df04b38a21337d3d0c6005c199705026
Danik <retikadni@gmail.com>
2025-05-01 22:00:25 +0200

    Lazy load network service

--- hypryou/hypryou_ui.py
+++ hypryou/hypryou_ui.py
@@ -22,7 +22,6 @@ from src.services import system_tray
 from src.services import cli
 from src.services import events
 from src.services import notifications
-from src.services import network
 
 # Modules
 from src.modules.bar import Bar, Corner
@@ -48,7 +47,6 @@ class HyprYou(gtk.Application):
 
     async def start_app(self) -> None:
         await hyprland.init()
-        network.get_network()
 
         try:
             utils.apply_css()
