2a19f231f64b4c49f1810f2ce304eaf096e73f4f
Danik <retikadni@gmail.com>
2025-07-17 16:27:22 +0200

    More templates

--- hypryou/src/modules/settings/base.py
+++ hypryou/src/modules/settings/base.py
@@ -35,7 +35,7 @@ class RowTemplate(gtk.Box):
         description: str | None,
         css_classes: tuple[str, ...] = (),
         clickable: bool = True,
-        **props: t.Any
+        **props: "RowTemplate.Props"
     ) -> None:
         super().__init__(
             css_classes=css_classes,
@@ -65,12 +65,14 @@ class RowTemplate(gtk.Box):
         self.set_description(description)
 
         self.append(self.info_box)
-        self.click_gesture = gtk.GestureClick.new()
-        self.click_gesture.set_button(0)
-        self.gesture_conn = (
-            self.click_gesture.connect("released", self.on_click_released)
-        )
-        self.add_controller(self.click_gesture)
+        self.clickable = clickable
+        if clickable:
+            self.click_gesture = gtk.GestureClick.new()
+            self.click_gesture.set_button(0)
+            self.gesture_conn = (
+                self.click_gesture.connect("released", self.on_click_released)
+            )
+            self.add_controller(self.click_gesture)
 
     def set_description(self, value: str | None) -> None:
         if value is not None:
@@ -108,8 +110,9 @@ class RowTemplate(gtk.Box):
             self.on_secondary_click()
 
     def destroy(self) -> None:
-        self.click_gesture.disconnect(self.gesture_conn)
-        self.remove_controller(self.click_gesture)
+        if self.clickable:
+            self.click_gesture.disconnect(self.gesture_conn)
+            self.remove_controller(self.click_gesture)
 
 
 class SwitchRowTemplate(RowTemplate):
@@ -148,55 +151,21 @@ class SwitchRowTemplate(RowTemplate):
         super().destroy()
 
 
-class SettingsBoolRow(SwitchRowTemplate):
-    __gtype_name__ = "SettingsBoolRow"
+class TextRowTemplate(RowTemplate):
+    __gtype_name__ = "SettingsTextRowTemplate"
 
     def __init__(
         self,
         label: str,
         description: str | None,
-        key: str,
-        css_classes: tuple[str, ...] = (),
-        **props: t.Any
-    ) -> None:
-        super().__init__(label, description, css_classes, **props)
-        self.key = key
-        self.settings = Settings()
-        self.settings_handler = self.settings.watch(
-            key, self.switch_set_active, True
-        )
-
-    def on_switch_changed(self, *args: t.Any) -> None:
-        self.settings.set(self.key, self.switch.get_active())
-
-    def destroy(self) -> None:
-        self.settings.unwatch(self.settings_handler)
-
-
-class SettingsTextRow(RowTemplate):
-    __gtype_name__ = "SettingsTextRow"
-
-    def __init__(
-        self,
-        label: str,
-        description: str | None,
-        key: str,
         left_icon: str | None = None,
         right_icon: str | None = None,
         max_length: int | None = None,
-        transform_fn: t.Callable[[t.Any], str] | None = None,
-        transform2_fn: t.Callable[[str], t.Any] | None = None,
-        test_text: t.Callable[[str], bool] | None = None,
         css_classes: tuple[str, ...] = (),
         max_width_chars: int | None = None,
         **props: t.Any
     ) -> None:
-        self.key = key
-        self.transform_fn = transform_fn
-        self.transform2_fn = transform2_fn
-        self.test_text = test_text
-        self.settings = Settings()
-        super().__init__(label, description, css_classes, **props)
+        super().__init__(label, description, css_classes, False, **props)
 
         self.entry_box = gtk.Box(
             css_classes=("entry-box",)
@@ -226,43 +195,17 @@ class SettingsTextRow(RowTemplate):
             self.entry_box.append(self.right_icon)
         self.append(self.entry_box)
 
-        self.debounced_text_changed = sync_debounce(500)(self.text_changed)
+        self.debounced_text_changed = sync_debounce(500)(self.on_text_changed)
         self.entry_handler = (
             self.entry.connect("notify::text", self.debounced_text_changed)
         )
-        self.settings_handler = self.settings.watch(
-            key, self.setting_updated
-        )
-
-    def setting_updated(self, new_value: t.Any) -> None:
-        value = (
-            self.transform_fn(new_value)
-            if self.transform_fn
-            else str(new_value)
-        )
-        if self.entry.get_text() != value:
-            self.entry.handler_block(self.entry_handler)
-            self.entry.set_text(value)
-            self.entry.handler_unblock(self.entry_handler)
 
     def destroy(self) -> None:
         super().destroy()
         self.entry.disconnect(self.entry_handler)
-        self.settings.unwatch(self.settings_handler)
 
-    def text_changed(self, *args: t.Any) -> None:
-        text = self.entry.get_text()
-        if self.test_text and not self.test_text(text):
-            toggle_css_class(self.entry_box, "incorrect", True)
-            return
-        toggle_css_class(self.entry_box, "incorrect", False)
-
-        value = (
-            self.transform2_fn(text)
-            if self.transform2_fn
-            else text
-        )
-        self.settings.set(self.key, value)
+    def on_text_changed(self, *args: t.Any) -> None:
+        ...
 
 
 class DropdownItem(gobject.Object):
@@ -278,20 +221,17 @@ class DropdownItem(gobject.Object):
         self.tooltip = tooltip
 
 
-class SettingsDropdownRow(RowTemplate):
-    __gtype_name__ = "SettingsDropdownRow"
+class DropdownRowTemplate(RowTemplate):
+    __gtype_name__ = "DropdownRowTemplate"
 
     def __init__(
         self,
         label: str,
         description: str | None,
-        key: str,
         items: list[DropdownItem],
         css_classes: tuple[str, ...] = (),
         **props: t.Any
     ) -> None:
-        self.key = key
-        self.settings = Settings()
         super().__init__(label, description, css_classes, **props)
 
         self.items = gio.ListStore.new(DropdownItem)
@@ -316,10 +256,6 @@ class SettingsDropdownRow(RowTemplate):
 
         self.append(self.dropdown)
 
-        self.settings_handler = self.settings.watch(
-            key, self.setting_updated
-        )
-
     def on_click(self) -> None:
         self.dropdown.activate()
 
@@ -342,23 +278,219 @@ class SettingsDropdownRow(RowTemplate):
         if item.tooltip:
             label.set_tooltip_text(item.tooltip)
 
-    def setting_updated(self, new_value: str) -> None:
+    def set_current(self, value: str) -> None:
         model = self.dropdown.get_model()
         for i in range(model.get_n_items()):
             item = t.cast(DropdownItem, model.get_item(i))
-            if item.value == new_value:
+            if item.value == value:
                 self.dropdown.set_selected(i)
                 break
+
+    def get_current(self) -> DropdownItem:
+        return t.cast(DropdownItem, self.dropdown.get_selected_item())
 
     def destroy(self) -> None:
         super().destroy()
         for handler in self.factory_handlers:
             self.factory.disconnect(handler)
         self.dropdown.disconnect(self.dropdown_handler)
+
+    def on_item_selected(self, *args: t.Any) -> None:
+        ...
+
+
+class Row(RowTemplate):
+    __gtype_name__ = "SettingsRow2"
+
+    def __init__(
+        self,
+        label: str,
+        description: str,
+        css_classes: tuple[str, ...] = (),
+        clickable: bool | None = None,
+        on_click: t.Callable[[t.Self], None] | None = None,
+        on_secondary_click: t.Callable[[t.Self], None] | None = None,
+        **props: t.Any
+    ):
+        if clickable is None:
+            clickable = on_click is not None or on_secondary_click is not None
+        self._on_click = on_click
+        self._on_secondary_click = on_secondary_click
+        super().__init__(label, description, css_classes, clickable, **props)
+
+    def on_click(self) -> None:
+        super().on_click()
+        if callable(self._on_click):
+            self._on_click(self)
+
+    def on_secondary_click(self) -> None:
+        super().on_secondary_click()
+        if callable(self._on_secondary_click):
+            self._on_secondary_click(self)
+
+
+class TextRow(TextRowTemplate):
+    __gtype_name__ = "SettingsTextRow2"
+
+    def __init__(
+        self,
+        label: str,
+        description: str | None,
+        left_icon: str | None = None,
+        right_icon: str | None = None,
+        max_length: int | None = None,
+        on_text_changed: t.Callable[[t.Self, str], None] = None,
+        css_classes: tuple[str, ...] = (),
+        max_width_chars: int | None = None,
+        **props: t.Any
+    ) -> None:
+        self._on_text_changed = on_text_changed
+        super().__init__(
+            label, description, left_icon, right_icon,
+            max_length, css_classes, max_width_chars,
+            **props
+        )
+
+    def on_text_changed(self, *args: t.Any):
+        if callable(self._on_text_changed):
+            self._on_text_changed(self, self.entry.get_text())
+
+
+class DropdownRow(DropdownRowTemplate):
+    __gtype_name__ = "SettingsDropdownRow2"
+
+    def __init__(
+        self,
+        label: str,
+        description: str | None,
+        items: list[DropdownItem],
+        css_classes: tuple[str, ...] = (),
+        on_selected: t.Callable[[t.Self, DropdownItem], None] | None = None,
+        **props: t.Any
+    ) -> None:
+        self._on_selected = on_selected
+        super().__init__(label, description, items, css_classes, **props)
+
+    def on_item_selected(self, *args: t.Any) -> None:
+        super().on_item_selected(*args)
+        if callable(self._on_selected):
+            self._on_selected(self, self.get_current())
+
+
+class SettingsBoolRow(SwitchRowTemplate):
+    __gtype_name__ = "SettingsBoolRow"
+
+    def __init__(
+        self,
+        label: str,
+        description: str | None,
+        key: str,
+        css_classes: tuple[str, ...] = (),
+        **props: t.Any
+    ) -> None:
+        super().__init__(label, description, css_classes, **props)
+        self.key = key
+        self.settings = Settings()
+        self.settings_handler = self.settings.watch(
+            key, self.switch_set_active, True
+        )
+
+    def on_switch_changed(self, *args: t.Any) -> None:
+        self.settings.set(self.key, self.switch.get_active())
+
+    def destroy(self) -> None:
+        self.settings.unwatch(self.settings_handler)
+
+
+class SettingsTextRow(TextRowTemplate):
+    __gtype_name__ = "SettingsTextRow"
+
+    def __init__(
+        self,
+        label: str,
+        description: str | None,
+        key: str,
+        left_icon: str | None = None,
+        right_icon: str | None = None,
+        max_length: int | None = None,
+        transform_fn: t.Callable[[t.Any], str] | None = None,
+        transform2_fn: t.Callable[[str], t.Any] | None = None,
+        test_text: t.Callable[[str], bool] | None = None,
+        css_classes: tuple[str, ...] = (),
+        max_width_chars: int | None = None,
+        **props: t.Any
+    ) -> None:
+        self.key = key
+        self.transform_fn = transform_fn
+        self.transform2_fn = transform2_fn
+        self.test_text = test_text
+        self.settings = Settings()
+        super().__init__(
+            label, description, left_icon, right_icon,
+            max_length, css_classes, max_width_chars,
+            **props
+        )
+
+        self.settings_handler = self.settings.watch(
+            key, self.on_setting_updated
+        )
+
+    def on_setting_updated(self, new_value: t.Any) -> None:
+        value = (
+            self.transform_fn(new_value)
+            if self.transform_fn
+            else str(new_value)
+        )
+        if self.entry.get_text() != value:
+            self.entry.handler_block(self.entry_handler)
+            self.entry.set_text(value)
+            self.entry.handler_unblock(self.entry_handler)
+
+    def destroy(self) -> None:
+        super().destroy()
+        self.settings.unwatch(self.settings_handler)
+
+    def on_text_changed(self, *args: t.Any) -> None:
+        text = self.entry.get_text()
+        if self.test_text and not self.test_text(text):
+            toggle_css_class(self.entry_box, "incorrect", True)
+            return
+        toggle_css_class(self.entry_box, "incorrect", False)
+
+        value = (
+            self.transform2_fn(text)
+            if self.transform2_fn
+            else text
+        )
+        self.settings.set(self.key, value)
+
+
+class SettingsDropdownRow(DropdownRowTemplate):
+    __gtype_name__ = "SettingsDropdownRow"
+
+    def __init__(
+        self,
+        label: str,
+        description: str | None,
+        key: str,
+        items: list[DropdownItem],
+        css_classes: tuple[str, ...] = (),
+        **props: t.Any
+    ) -> None:
+        self.key = key
+        self.settings = Settings()
+        super().__init__(label, description, items, css_classes, **props)
+
+        self.settings_handler = self.settings.watch(
+            key, self.set_current
+        )
+
+    def destroy(self) -> None:
+        super().destroy()
         self.settings.unwatch(self.settings_handler)
 
     def on_item_selected(self, *args: t.Any) -> None:
-        item = t.cast(DropdownItem, self.dropdown.get_selected_item())
+        item = self.get_current()
         self.settings.set(self.key, item.value)
 
 
