03825dad538f15a35c76d15bbd9149aa69fb0cf4
Paweł <134298056+FixeQyt@users.noreply.github.com>
2025-08-02 16:33:01 +0200

    Update hypryou_ui.py - refactor: remove LidService alias and import LidMonitorService directly

--- hypryou/hypryou_ui.py
+++ hypryou/hypryou_ui.py
@@ -44,7 +44,7 @@ from src.services.audio import AudioService
 from src.services.clock import ClockService
 from src.services.network import NetworkService
 from src.services.bluetooth_agent import BluetoothAgentService
-from src.services.lid import LidService
+from src.services.lid import LidMonitorService
 
 import src.services.cliphist as cliphist
 
@@ -93,7 +93,7 @@ services: tuple[AsyncService | Service, ...] = (
     AudioService(),
     ClockService(),
     BluetoothAgentService(),
-    LidService()
+    LidMonitorService()
 )
 
 popups_types = (
