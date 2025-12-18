4cb749f6115dfa7207c593abe8e621a8d67c073e
Danik <retikadni@gmail.com>
2025-12-18 18:05:57 +0100

    Order it better so it's more readable

--- hypryou/src/services/hyprland_keybinds/actions.py
+++ hypryou/src/services/hyprland_keybinds/actions.py
@@ -2,27 +2,9 @@ from src.services.hyprland_keybinds.common import KeyBind, main_mod, Category
 
 key_binds = (
     KeyBind(
-        (main_mod, "SHIFT", "A"),
-        ("exec", "hypryouctl toggle_animations"),
-        "Toggle all animations",
-        Category.ACTIONS
-    ),
-    KeyBind(
         (main_mod, "SHIFT", "S"),
         ("exec", "hypryouctl screenshot region"),
         "Screenshot",
-        Category.ACTIONS
-    ),
-    KeyBind(
-        (main_mod, "CTRL", "S"),
-        ("exec", "hypryouctl screenshot window"),
-        "Screenshot of window",
-        Category.ACTIONS
-    ),
-    KeyBind(
-        (main_mod, "ALT", "S"),
-        ("exec", "hypryouctl screenshot active"),
-        "Screenshot of active screen",
         Category.ACTIONS
     ),
     KeyBind(
@@ -32,15 +14,33 @@ key_binds = (
         Category.ACTIONS
     ),
     KeyBind(
+        (main_mod, "CTRL", "S"),
+        ("exec", "hypryouctl screenshot window"),
+        "Screenshot of window",
+        Category.ACTIONS
+    ),
+    KeyBind(
         (main_mod, "CTRL", "F"),
         ("exec", "hypryouctl screenshot window freeze"),
         "Screenshot of window and freeze",
         Category.ACTIONS
     ),
     KeyBind(
+        (main_mod, "ALT", "S"),
+        ("exec", "hypryouctl screenshot active"),
+        "Screenshot of active screen",
+        Category.ACTIONS
+    ),
+    KeyBind(
         (main_mod, "ALT", "F"),
         ("exec", "hypryouctl screenshot active freeze"),
         "Screenshot of active screen and freeze",
+        Category.ACTIONS
+    ),
+    KeyBind(
+        (main_mod, "SHIFT", "A"),
+        ("exec", "hypryouctl toggle_animations"),
+        "Toggle all animations",
         Category.ACTIONS
     ),
     KeyBind(
