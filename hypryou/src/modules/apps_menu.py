48d9afd798d83143f07a46d380a53120777675ed
Heaven <>
2026-04-14 15:59:22 +0200

    Fixed the close when you open an app

--- hypryou/src/modules/apps_menu.py
+++ hypryou/src/modules/apps_menu.py
@@ -1,15 +1,17 @@
-from functools import lru_cache
 import heapq
 import math
-from repository import gtk, gdk, layer_shell, glib, pango, gio
-from src.services.apps import Application, apps, reload as apps_reload
-from utils.debounce import sync_debounce
-from utils.styles import toggle_css_class
-from utils.logger import logger
-import weakref
 import typing as t
-from src.services.state import close_window
+import weakref
+from functools import lru_cache
+
+from repository import gdk, gio, glib, gtk, layer_shell, pango
 from src import widget
+from src.services.apps import Application, apps
+from src.services.apps import reload as apps_reload
+from src.services.state import close_window
+from utils.debounce import sync_debounce
+from utils.logger import logger
+from utils.styles import toggle_css_class
 
 
 @lru_cache(512)
@@ -19,9 +21,12 @@ def cache_icon(icon: gio.Icon | None) -> gtk.IconPaintable | None:
 
     if icon is None:
         texture = icon_theme.lookup_icon(
-            "application-x-executable", None, 32, 1,
+            "application-x-executable",
+            None,
+            32,
+            1,
             gtk.TextDirection.LTR,
-            gtk.IconLookupFlags.FORCE_REGULAR
+            gtk.IconLookupFlags.FORCE_REGULAR,
         )
     else:
         texture = icon_theme.lookup_by_gicon(
@@ -34,9 +39,12 @@ def cache_icon(icon: gio.Icon | None) -> gtk.IconPaintable | None:
 
         if texture is None:
             texture = icon_theme.lookup_icon(
-                "application-x-executable", None, 32, 1,
+                "application-x-executable",
+                None,
+                32,
+                1,
                 gtk.TextDirection.LTR,
-                gtk.IconLookupFlags.FORCE_REGULAR
+                gtk.IconLookupFlags.FORCE_REGULAR,
             )
     return texture
 
@@ -46,30 +54,24 @@ class AppItem(gtk.Revealer):
 
     def __init__(self, item: Application) -> None:
         self.on_click = sync_debounce(750, 1, True)(self._on_click)
-        self.box = gtk.Box(
-            css_classes=("app-item-box",)
-        )
+        self.box = gtk.Box(css_classes=("app-item-box",))
         self.button = gtk.Button(
             css_classes=("app-item",),
             child=self.box,
-            tooltip_text=f"{item.name}\n{item.description or ""}"
+            tooltip_text=f"{item.name}\n{item.description or ''}",
         )
         super().__init__(
             css_classes=("app-item-revealer",),
             child=self.button,
             transition_duration=250,
             transition_type=gtk.RevealerTransitionType.SLIDE_DOWN,
-            reveal_child=True
+            reveal_child=True,
         )
         self.item = item
 
-        self.icon = gtk.Picture(
-            css_classes=("icon",)
-        )
+        self.icon = gtk.Picture(css_classes=("icon",))
         self.label = gtk.Label(
-            css_classes=("label",),
-            label=item.name,
-            ellipsize=pango.EllipsizeMode.END
+            css_classes=("label",), label=item.name, ellipsize=pango.EllipsizeMode.END
         )
 
         texture = cache_icon(item.icon)
@@ -80,9 +82,7 @@ class AppItem(gtk.Revealer):
         self.box.append(self.label)
 
         self.on_click_handler = self.button.connect("clicked", self.on_click)
-        self.on_activate_handler = self.button.connect(
-            "activate", self._on_click
-        )
+        self.on_activate_handler = self.button.connect("activate", self._on_click)
 
     def _on_click(self, *args: t.Any) -> None:
         self.launch()
@@ -106,35 +106,31 @@ class AppsBox(gtk.Box):
             css_classes=("apps-box",),
             orientation=gtk.Orientation.VERTICAL,
             vexpand=True,
-            halign=gtk.Align.FILL
+            halign=gtk.Align.FILL,
         )
         self.list = gtk.Box(
             css_classes=("apps-list",),
             orientation=gtk.Orientation.VERTICAL,
-            vexpand=True
+            vexpand=True,
         )
         self.scrollable = gtk.ScrolledWindow(
             css_classes=("apps-scrollable",),
             hscrollbar_policy=gtk.PolicyType.NEVER,
             vscrollbar_policy=gtk.PolicyType.AUTOMATIC,
             child=self.list,
-            vexpand=True
+            vexpand=True,
         )
 
-        self.search_box = gtk.Box(
-            css_classes=("misc--search", "search")
-        )
+        self.search_box = gtk.Box(css_classes=("misc--search", "search"))
         self.entry_icon = widget.Icon("search")
         self.entry = gtk.Entry(
-            css_classes=("entry",),
-            placeholder_text="Search",
-            hexpand=True
+            css_classes=("entry",), placeholder_text="Search", hexpand=True
         )
         self.search_box.append(self.entry_icon)
         self.search_box.append(self.entry)
         self.entry_handlers = (
             self.entry.connect("notify::text", self.on_search),
-            self.entry.connect("activate", self.on_entry_enter)
+            self.entry.connect("activate", self.on_entry_enter),
         )
 
         self._apps: dict[Application, AppItem] = {}
@@ -152,7 +148,7 @@ class AppsBox(gtk.Box):
 
     def on_entry_enter(self, *args: t.Any) -> None:
         if self.last_highest:
-            self.last_highest.item.launch()
+            self.last_highest.launch()
 
     @sync_debounce(150)
     def on_search(self, *args: t.Any) -> None:
@@ -162,7 +158,9 @@ class AppsBox(gtk.Box):
                 item.set_reveal_child(True)
 
         if len(text.strip()) > 0:
-            scores: dict[AppItem, int] = {app: app.item.match(text) for app in self._apps.values()}
+            scores: dict[AppItem, int] = {
+                app: app.item.match(text) for app in self._apps.values()
+            }
             top_search = heapq.nlargest(8, scores.items(), key=lambda item: item[1])
             search = {app: score for app, score in top_search}
             for item in self._apps.values():
@@ -186,8 +184,8 @@ class AppsBox(gtk.Box):
         for item, score in top_search.items():
             if item.get_reveal_child() and (
                 not highest
-                or score + 0.02 * math.log1p(item.item.frequency) >
-                top_search[highest] + 0.02 * math.log1p(highest.item.frequency)
+                or score + 0.02 * math.log1p(item.item.frequency)
+                > top_search[highest] + 0.02 * math.log1p(highest.item.frequency)
             ):
                 highest = item
 
@@ -205,8 +203,7 @@ class AppsBox(gtk.Box):
     def sort_by_frequent(self) -> None:
         new_dict = dict(
             sorted(
-                self._apps.items(),
-                key=lambda item: (item[0].frequency, item[0].name)
+                self._apps.items(), key=lambda item: (item[0].frequency, item[0].name)
             )
         )
         self._apps = new_dict
@@ -242,10 +239,7 @@ class AppsWindow(widget.LayerWindow):
     def __init__(self, app: gtk.Application) -> None:
         super().__init__(
             app,
-            anchors={
-                "top": True,
-                "left": True
-            },
+            anchors={"top": True, "left": True},
             css_classes=("apps-menu",),
             keymode=layer_shell.KeyboardMode.ON_DEMAND,
             layer=layer_shell.Layer.OVERLAY,
@@ -253,14 +247,12 @@ class AppsWindow(widget.LayerWindow):
             name="apps_menu",
             height=400,
             width=400,
-            setup_popup=True
+            setup_popup=True,
         )
         self.name = "apps_menu"
         self._child: AppsBox | None = None
         if __debug__:
-            weakref.finalize(
-                self, lambda: logger.debug("AppsWindow finalized")
-            )
+            weakref.finalize(self, lambda: logger.debug("AppsWindow finalized"))
 
     def on_show(self) -> None:
         glib.idle_add(apps_reload)
