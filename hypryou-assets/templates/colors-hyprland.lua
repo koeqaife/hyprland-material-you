2dc7cefbca75511ab254d3a9d56bd9780f51a8ff
Danik <retikadni@gmail.com>
2026-08-16 03:37:14 +0200

    Use Lua for colors

---
+++ hypryou-assets/templates/colors-hyprland.lua
@@ -0,0 +1,11 @@
+hl.config({
+    general = {
+        col = {
+            active_border = "rgb(<primary.rgb>)",
+            inactive_border = "rgb(<outlineVariant.rgb>)"
+        }
+    },
+    misc = {
+        background_color = "rgb(<surfaceContainerLowest.rgb>)"
+    }
+})
