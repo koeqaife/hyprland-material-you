6f82aaf051d08f2ef501fb09a976f82c647fde40
Danik <retikadni@gmail.com>
2026-08-16 04:32:16 +0200

    Doesnt work *shrugs*

--- hypryou/src/services/hyprland_keybinds/apps.py
+++ hypryou/src/services/hyprland_keybinds/apps.py
@@ -33,11 +33,5 @@ key_binds = (
         make_exec("hypryouctl apps files"),
         "File Manager",
         Category.APPS
-    ),
-    KeyBind(
-        (main_mod, "SHIFT", "L"),
-        make_exec("sh -c \"TERM=$(hypryouctl apps terminal); $TERM -e sh -c 'btop; read' & sleep 0.1; $TERM -e sh -c 'fastfetch; read' & sleep 0.1; $TERM -e sh -c 'htop; read' & sleep 0.1; $TERM -e sh -c 'cava; read' & sleep 0.1; $TERM -e sh -c 'cmatrix; read'\""),
-        "Launch system dashboard",
-        Category.APPS
     )
 )
